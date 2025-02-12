608a408840c27b57d57fbf0a7f2a893b477f1f84
Artemius <artemcka.jdanov@yandex.ru>
2025-02-12 23:44:49 +0700

    Add user models

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,34 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from typing import List
 
 app = FastAPI()
 
-# Настройка CORS
+#Модель для пользователя 
+class User(BaseModel):
+    id:str
+    username:str
+    password_hash:str
+
+#Модель для пользователя 
+class UserRegsiter(BaseModel):
+    id:str
+    username:str
+
+
+#Модель для пользователя 
+class UserLogin(BaseModel):
+    id:str
+    username:str
+   
+#Модель для пользователя 
+class Token(BaseModel):
+    token:str
+    token_type:str
+
+#Настройка CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
