6aaff320eaaad9dbc48d95430ed6b5331cf9a56f
Artemius <artemcka.jdanov@yandex.ru>
2025-02-16 20:24:05 +0700

    Change Users models, add create token function

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from typing import List, Optional
+import uuid
+import datetime
+
 
 from typing import List
 
@@ -14,39 +20,43 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-#Модель для пользователя 
-class User(BaseModel):
+class ShortUser(BaseModel):
     id:str
     username:str
+
+class FullUser(ShortUser):
     password_hash:str
 
-#Модель для пользователя 
 class UserRegsiter(BaseModel):
     id:str
     username:str
 
-
-#Модель для пользователя 
 class UserLogin(BaseModel):
     id:str
     username:str
    
-#Модель для пользователя 
 class Token(BaseModel):
     token:str
     token_type:str
 
-
 users_db: dict = {}
 
-def get_user(username: str):
+def create_token(data: dict, expires_delta: Optional[datetime.timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_user(username: str) -> Optional[FullUser]:
     if username in users_db:
         user_dict = users_db[username]
-        return User(**user_dict)
+        return FullUser(**user_dict)
     return None
-
-
 
 #Настройка CORS
 app.add_middleware(
